a525232ddb9b193a2a3ee91259ef23f6375bfc72
SergioCubasB <50562996+SergioCubasB@users.noreply.github.com>
2019-05-15 11:43:12 -0500

    Reajuste para menu

--- assets/js/logica.js
+++ assets/js/logica.js
@@ -21,8 +21,29 @@ Element.prototype.addClass = function(c){
     return this.addEventListener(h,f);
   }
 
+
 var d = $('#somedialog');
 $('.open').on('click',function(e){
+  e.preventDefault();
+  d.removeClass('dialog-close');
+  d.addClass('dialog-open');
+});
+$('.dialog-overlay').on('click',function(e){
+  e.preventDefault();
+  d.removeClass('dialog-open');
+  d.addClass('dialog-close');
+});
+$('.btn-cta').on('click',function(e){
+  e.preventDefault();
+  /*$('.dialog-content').innerHTML='<i>Thank you for your honesty!';*/
+  setTimeout(()=>{ 
+  d.removeClass('dialog-open');
+  d.addClass('dialog-close');},10);
+});
+
+
+var d = $('#somedialog');
+$('.open2').on('click',function(e){
   e.preventDefault();
   d.removeClass('dialog-close');
   d.addClass('dialog-open');
